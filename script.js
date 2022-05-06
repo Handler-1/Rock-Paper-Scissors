ca7247baab943439f25ba86d49bb736aa9935ef7
JHdev123 <102925671+JHdev123@users.noreply.github.com>
2022-05-06 09:54:58 -0400

    Second Commit (draft)

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const queryElement = (query) => document.querySelector(query);
 
+const playerPoints = document.querySelector('#playerScore');
+const computerPoints = document.querySelector('#computerScore');
 const rockEl = queryElement("#rock");
 const paperEl = queryElement("#paper");
 const scissorsEl = queryElement("#scissors");
@@ -7,40 +9,46 @@ const scissorsEl = queryElement("#scissors");
 const choices = [rockEl, paperEl, scissorsEl];
 
 function onSelection() {
-  console.log(this.value);
-}
+    console.log(this.value);
+  }
 
-choices.forEach((choice) => {
-  choice.addEventListener("click", onSelection);
-});
+let compScore = 0;
+let playerScore = 0;
 
 function computerPlay() {
     let choices = ["rock", "paper", "scissors"];
     return choices[Math.floor(choices.length * Math.random())];
 } 
 
-function playRound(playerSelection) {
-    let computerSelection = computerPlay;
+function playRound() {
+    let playerSelection = onSelection();
+    let computerSelection = computerPlay();
     
     if (playerSelection === "Rock" && computerSelection === "Scissors") {
+        playerPoints.textContent = ++playerPoints
         return("You Win!");
     } else if (playerSelection === "Rock" && computerSelection === "Paper") {
+        computerPoints.textContent = ++computerPoints
         return("You Lose!");
     } else if (playerSelection === "Rock" && computerSelection === "Rock") {
         return("It's A Tie!");
     }
     
     else if (playerSelection === "Paper" && computerSelection === "Rock") {
+        playerPoints.textContent = ++playerPoints
         return("You Win!");
     } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+        computerPoints.textContent = ++computerPoints
         return("You Lose!");
     } else if (playerSelection === "Paper" && computerSelection === "Paper") {
         return("It's A Tie!");
     }
     
     else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+        playerPoints.textContent = ++playerPoints
         return("You Win!");
     } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+        computerPoints.textContent = ++computerPoints
         return("You Lose!");
     } else if (playerSelection === "Scissors" && computerSelection === "Scissors") {
         return("It's A Tie!");
@@ -48,12 +56,25 @@ function playRound(playerSelection) {
         return("Uh oh, something went wrong");
 }
 
-document.getElementById('rock').onclick = playerSelection;
-document.getElementById('paper').onclick = playerSelection;
-document.getElementById('scissors').onclick = playerSelection;
-
-// console.log(playRound(playerSelection)); //
 
 function game() {
-    let (i=0, i < 5, i++)
+    playRound(playerSelection, computerSelection);
+    if (playerPoints < 5 && computerPoints < 5) {
+        game()
+    } else {
+        gameEnd();
+    }
 }
+
+function gameEnd() {
+    if (playerPoints > computerPoints) {
+        console.log("Game Over! You Win!");
+    } else if (computerPoints > playerPoints) {
+        console.log("Game Over! You Lose!")
+    }
+}
+
+choices.forEach((choice) => {
+    choice.addEventListener("click", onSelection);
+  });
+
