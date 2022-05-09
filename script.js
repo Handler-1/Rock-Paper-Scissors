c32fdc3aa62cc0df7f1e03259b66ceed529a4d6d
JHdev123 <102925671+JHdev123@users.noreply.github.com>
2022-05-09 00:05:50 -0400

    Fixed game functionalities
    Game is now working, score tracking is fixed, and ends game when one player reaches 5 points.

--- script.js
+++ script.js
@@ -1,80 +1,194 @@
 const queryElement = (query) => document.querySelector(query);
 
-const playerPoints = document.querySelector('#playerScore');
-const computerPoints = document.querySelector('#computerScore');
 const rockEl = queryElement("#rock");
 const paperEl = queryElement("#paper");
 const scissorsEl = queryElement("#scissors");
 
 const choices = [rockEl, paperEl, scissorsEl];
 
-function onSelection() {
-    console.log(this.value);
-  }
-
-let compScore = 0;
-let playerScore = 0;
-
 function computerPlay() {
-    let choices = ["rock", "paper", "scissors"];
+    let choices = ["rock", "paper", "scissors"]; 
     return choices[Math.floor(choices.length * Math.random())];
 } 
 
-function playRound() {
-    let playerSelection = onSelection();
-    let computerSelection = computerPlay();
+function onSelection() {
+    playRound(this.value);
+}
+
+let playerPoints = document.querySelector('#playerScore');
+let computerPoints = document.querySelector('#computerScore');  
+
+function playRound(choice) {
+    let playerSelection = choice;
+    let computerSelection = computerPlay(); 
     
-    if (playerSelection === "Rock" && computerSelection === "Scissors") {
-        playerPoints.textContent = ++playerPoints
-        return("You Win!");
-    } else if (playerSelection === "Rock" && computerSelection === "Paper") {
-        computerPoints.textContent = ++computerPoints
-        return("You Lose!");
-    } else if (playerSelection === "Rock" && computerSelection === "Rock") {
-        return("It's A Tie!");
+    if (playerSelection === "rock" && computerSelection === "scissors") {
+        updateChoices(playerSelection, playerSign);
+        updateChoices(computerSelection, computerSign);
+        playerPoints.textContent = Number(playerPoints.textContent) + 1;
+        scoreInfo.textContent = 'You won!';
+
+        if (playerPoints.textContent == 5) {
+            scoreInfo.textContent = 'You won the game!';
+            disableButtons();
+        } else if (computerPoints.textContent == 5) {
+            scoreInfo.textContent = 'You lost the game!';
+            disableButtons();
+        }
+
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        updateChoices(playerSelection, playerSign);
+        updateChoices(computerSelection, computerSign);
+        computerPoints.textContent = Number(computerPoints.textContent) + 1;
+        scoreInfo.textContent = 'You lost!';
+
+        if (playerPoints.textContent == 5) {
+            scoreInfo.textContent = 'You won the game!';
+            disableButtons();
+        } else if (computerPoints.textContent == 5) {
+            scoreInfo.textContent = 'You lost the game!';
+            disableButtons();
+        }
+        
+    } else if (playerSelection === "rock" && computerSelection === "rock") {
+        updateChoices(playerSelection, playerSign);
+        updateChoices(computerSelection, computerSign);
+        scoreInfo.textContent = "It's a tie!";
     }
     
-    else if (playerSelection === "Paper" && computerSelection === "Rock") {
-        playerPoints.textContent = ++playerPoints
-        return("You Win!");
-    } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
-        computerPoints.textContent = ++computerPoints
-        return("You Lose!");
-    } else if (playerSelection === "Paper" && computerSelection === "Paper") {
-        return("It's A Tie!");
+    else if (playerSelection === "paper" && computerSelection === "rock") {
+        updateChoices(playerSelection, playerSign);
+        updateChoices(computerSelection, computerSign);
+        playerPoints.textContent = Number(playerPoints.textContent) + 1;
+        scoreInfo.textContent = 'You won!';
+
+        if (playerPoints.textContent == 5) {
+            scoreInfo.textContent = 'You won the game!';
+            disableButtons();
+        } else if (computerPoints.textContent == 5) {
+            scoreInfo.textContent = 'You lost the game!';
+            disableButtons();
+        }
+
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        updateChoices(playerSelection, playerSign);
+        updateChoices(computerSelection, computerSign);
+        computerPoints.textContent = Number(computerPoints.textContent) + 1;
+        scoreInfo.textContent = 'You lost!';
+
+        if (playerPoints.textContent == 5) {
+            scoreInfo.textContent = 'You won the game!';
+            disableButtons();
+        } else if (computerPoints.textContent == 5) {
+            scoreInfo.textContent = 'You lost the game!';
+            disableButtons();
+        }
+
+    } else if (playerSelection === "paper" && computerSelection === "paper") {
+        updateChoices(playerSelection, playerSign);
+        updateChoices(computerSelection, computerSign);
+        scoreInfo.textContent = "It's a tie!";
     }
     
-    else if (playerSelection === "Scissors" && computerSelection === "Paper") {
-        playerPoints.textContent = ++playerPoints
-        return("You Win!");
-    } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
-        computerPoints.textContent = ++computerPoints
-        return("You Lose!");
-    } else if (playerSelection === "Scissors" && computerSelection === "Scissors") {
-        return("It's A Tie!");
+    else if (playerSelection === "scissors" && computerSelection === "paper") {
+        updateChoices(playerSelection, playerSign);
+        updateChoices(computerSelection, computerSign);
+        playerPoints.textContent = Number(playerPoints.textContent) + 1;
+        scoreInfo.textContent = 'You won!';
+
+        if (playerPoints.textContent == 5) {
+            scoreInfo.textContent = 'You won the game!';
+            disableButtons();
+        } else if (computerPoints.textContent == 5) {
+            scoreInfo.textContent = 'You lost the game!';
+            disableButtons();
+        }
+
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        updateChoices(playerSelection, playerSign);
+        updateChoices(computerSelection, computerSign);
+        computerPoints.textContent = Number(computerPoints.textContent) + 1;
+        scoreInfo.textContent = 'You lost!';
+
+        if (playerPoints.textContent == 5) {
+            scoreInfo.textContent = 'You won the game!';
+            disableButtons();
+        } else if (computerPoints.textContent == 5) {
+            scoreInfo.textContent = 'You lost the game!';
+            disableButtons();
+        }
+
+    } else if (playerSelection === "scissors" && computerSelection === "scissors") {
+        updateChoices(playerSelection, playerSign);
+        updateChoices(computerSelection, computerSign);
+        scoreInfo.textContent = "It's a tie!";
     } else
         return("Uh oh, something went wrong");
 }
 
+const optButtons = document.getElementsByClassName('playerChoice');
+function disableButtons() {
+    Array.from(optButtons).forEach(button => {
+        button.removeEventListener('click', onSelection);
+    })
+} 
 
-function game() {
-    playRound(playerSelection, computerSelection);
-    if (playerPoints < 5 && computerPoints < 5) {
-        game()
-    } else {
-        gameEnd();
-    }
+/*
+const optButtons = document.getElementsByClassName('playerChoice');
+
+function disableButtons() {
+    optButtons.forEach(button => {
+        button.removeEventListener('click', onSelection);
+    })
+} 
+
+--The above does not work because what I am getting here is not an array, but an HTMLCollection. 
+It is array-like, but not an array. HTMLCollections do not have access to native methods like.map and
+.forEach (these belong to array.prototype)
+
+es6 method:
+const optButtons = document.getElementsByClassName("playerChoice");
+
+function disableButtons() {
+  [...optButtons].forEach((button) => {
+    button.removeEventListener("click", onSelection);
+  });
 }
 
-function gameEnd() {
-    if (playerPoints > computerPoints) {
-        console.log("Game Over! You Win!");
-    } else if (computerPoints > playerPoints) {
-        console.log("Game Over! You Lose!")
-    }
-}
+--this is using the spread syntax (it's ...). it's basically listing out every single item
+in optButtons and then fitting it into an array []
+*/
 
 choices.forEach((choice) => {
     choice.addEventListener("click", onSelection);
-  });
+});
+
+const resetButton = document.querySelector('#reset');
+
+resetButton.addEventListener('click',() => location.reload());
+
+const scoreInfo = document.getElementById('scoreInfo')
+const playerSign = document.getElementById('playerSign')
+const computerSign = document.getElementById('computerSign')
+
+function updateChoices(selection, element) {
+    switch (selection) {
+      case "rock":
+        element.textContent = "✊";
+        break;
+      case "paper":
+        element.textContent = "✋";
+        break;
+      case "scissors":
+        element.textContent = "✌";
+        break;
+    }
+  }
+
+  /* 
+  element?.textContent = "✊"; 
+  --This is to ensure that the code won't break when calling with something that isn't an html element.
+  the ? in this case is called optional chaining. So if the argument passed does have
+  .textContent, it will set it to whatever. If it doesn't, it won't bother (it won't raise any errors) 
+  */
 
